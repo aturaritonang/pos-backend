11306574bcc3e902fa9d89a9011b1956c2f45c6d
Atur Aritonang <mangatur.aritonang@gmail.com>
2024-11-21 10:17:55 +0700

    Add Dto

---
+++ XsisPos/XsisPos.Dto/CategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace XsisPos.Dto
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Initial { get; set; }
+        public string Name { get; set; }
+    }
+}
